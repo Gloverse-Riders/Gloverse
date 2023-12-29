2445cebe7850c08588916d525fe70f6b9f4f3a47
Gabriela-Daniela Laurus <gabriela.laurus@student.upt.ro>
2023-12-29 17:09:26 +0200

    menu-items-started

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -1,16 +1,26 @@
-import {MenuItem} from "@/app/models/MenuItem";
-import mongoose from "mongoose";
 
-export async function POST(req){
+
+import mongoose from "mongoose";
+import {MenuItem} from "@/app/models/MenuItem";
+
+export async function POST(req) {
     mongoose.connect(process.env.MONGO_URL);
-    const data = req.json();
-    const menuItemDoc = await MenuItem.create(data);
-    return Response.json(menuItemDoc);
+    const data = await req.json();
+        const menuItemDoc = await MenuItem.create(data);
+        return Response.json(menuItemDoc);
 }
 
-export async function GET(){
+export async function PUT(req) {
+    mongoose.connect(process.env.MONGO_URL);
+        const {_id, ...data} = await req.json();
+        await MenuItem.findByIdAndUpdate(_id, data);
+    return Response.json(true);
+}
+
+export async function GET() {
     mongoose.connect(process.env.MONGO_URL);
     return Response.json(
         await MenuItem.find()
     );
 }
+
